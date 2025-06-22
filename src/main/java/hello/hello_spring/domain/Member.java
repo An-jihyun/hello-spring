678812db6cac89a97117003757edb844e025f926
An-jihyun <anjh322@gmail.com>
2025-06-22 15:52:59 +0900

    Initial commit

---
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hello_spring.domain;
+
+public class Member {
+    private Long id; //시스템이 저장하기 위해 사용되는 ID
+    private String name; //이름
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private  static Map<Long, Member> store = new HashMap<>();
+    //이건 간단한 예제니까 해쉬맵을 쓰지만 실무에서는 이게 동시성 문제가 발생할 수 있어서
+    //이렇게 공유되는 변수일때는 Concurrent hashMap을 사용
+    private static long sequence = 0L;
+    //얘도 실무에선 이렇게 하는거보다는 동시성 문제 고려해서
+    //atomioc long등을 해야하는데 일단 가장 단순하게 함
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
